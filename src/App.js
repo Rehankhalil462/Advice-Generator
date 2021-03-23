6bec770754293fe4957a28e924b5c96193add9cb
Rehan Khalil <rehankhalil462@gmail.com>
2021-03-24 03:24:23 +0500

    created a advice-generator progressive web app in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState(null);
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  const getAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        setAdvice(response.data.slip.advice);
+        console.log('i am fired');
+      })
+      .catch((error) => console.log(error));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='card'>
+        <h1 className='heading'>{advice}</h1>
+        <button className='button' onClick={getAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
