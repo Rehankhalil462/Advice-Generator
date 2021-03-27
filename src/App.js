3d634562a2bfa3d08ede343f8a8b7bba6f045b05
Rehan Khalil <rehankhalil462@gmail.com>
2021-03-28 04:44:38 +0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import Footer from './Footer/Footer';
 
 const App = () => {
   const [advice, setAdvice] = useState(null);
@@ -19,14 +20,17 @@ const App = () => {
       .catch((error) => console.log(error));
   };
   return (
-    <div className='app'>
-      <div className='card'>
-        <h1 className='heading'>{advice}</h1>
-        <button className='button' onClick={getAdvice}>
-          <span>GIVE ME ADVICE!</span>
-        </button>
+    <>
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={getAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 export default App;
